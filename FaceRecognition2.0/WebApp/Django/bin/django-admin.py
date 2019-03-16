22eb2770c35a014babdf6c28f647fb99137592e4
Sreerag R Nandan <sreeragraghunandan@gmail.com>
2019-03-16 13:41:44 +0530

    addd django

---
+++ FaceRecognition2.0/WebApp/Django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sreerag/Desktop/python/face_recognition/FaceRecognition2.0/WebApp/Django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ FaceRecognition2.0/sql.py
@@ -0,0 +1,32 @@
+
+import pymysql
+import re
+#database connection
+connection = pymysql.connect(host="localhost",port=3307, user="root", passwd="", database="face_recognition")
+cursor = connection.cursor()
+
+# queries for inserting values
+retrive = "SELECT name FROM observations WHERE id=(SELECT max(id) FROM observations);"
+#executing the quires
+
+cursor.execute(retrive)
+Featched_name = cursor.fetchall()
+
+retriveCAM = "SELECT cam FROM observations WHERE id=(SELECT max(id) FROM observations);"
+cursor.execute(retriveCAM)
+Featched_cam = cursor.fetchall()
+#commiting the connection then closing it.
+connection.commit()
+connection.close()
+name ="sreerag"
+camid = "c1"
+print(Featched_cam)
+cam = ','.join(map(','.join,Featched_cam))
+print (cam)
+obsName = ','.join(map(','.join,Featched_name))
+print (obsName)
+if name !="Unknown" and (name != obsName or camid != cam):
+    print (obsName)
+# result = re.sub('abc(def)ghi','',Featched_name )
+# print (result)
+# print re.sub('[^a-zA-Z]+', r'', Featched_name)
